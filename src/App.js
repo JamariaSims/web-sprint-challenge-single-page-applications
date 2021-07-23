c6cf339c93613fa3b3df3299f02d6bd8cc2f7892
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-23 15:14:05 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "./components/Form";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
 	const [data, setData] = useState([]);
