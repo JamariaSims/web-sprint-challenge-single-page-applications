ca52e9bffffb2a0c747426c439b9f1dcad2d2391
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-23 14:55:15 -0400

    update5

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -5,13 +5,13 @@ describe("example to-do app", () => {
 
 	it("Check for text input", () => {
 		cy.get('[id="name-input"]').then(($input) => {
-			const input = $input.data;
-			console.log($input.data);
+			const input = $input;
+			.and("hht",value)
 		});
 		cy.get(`[id = "order-button"]`)
 			.click()
 			.then(($res) => {
-				return $res;
+				console.log($res);
 			});
 	});
 });
