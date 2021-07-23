8838beba19d689e9c90da7817a472bec074535f4
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-23 13:30:42 -0400

    Update1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
+	return (
+		<div className="app">
+			<nav>
+				<h1>LAMBDA EATS</h1>
+				<button>Home</button>
+				<button>Help</button>
+			</nav>
+			<div className="con1">
+				<img />
+				<button>Pizza?</button>
+			</div>
+			<div className="con2"></div>
+			<div className="con3"></div>
+		</div>
+	);
 };
 export default App;
