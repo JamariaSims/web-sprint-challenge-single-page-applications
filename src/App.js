0ce75012b19dd88cac238bc5882ab0176bfc4590
Jamaria Sims <jamariasims@Jamarias-MacBook-Air.local>
2021-07-23 14:59:17 -0400

    update5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "./components/Form";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
 	const [data, setData] = useState([]);
@@ -10,15 +11,22 @@ function App() {
 			.then((res) => setData(res.data.data));
 	}, []);
 	return (
-		<div className="App">
-			<Form data={data} setData={setData} />
-			{data &&
-				data.map((x, y) => (
-					<>
-						<p>{JSON.stringify(x)}</p>
-					</>
-				))}
-		</div>
+		<Router>
+			{" "}
+			<Route path="/">
+				<div className="App">
+					{data &&
+						data.map((x, y) => (
+							<>
+								<p>{JSON.stringify(x)}</p>
+							</>
+						))}
+				</div>
+			</Route>
+			<Route path="/form">
+				<Form data={data} setData={setData} />
+			</Route>
+		</Router>
 	);
 }
 export default App;
